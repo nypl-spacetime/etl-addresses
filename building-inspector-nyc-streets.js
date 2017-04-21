653b0e9f485387393156ba5b4fd50de0fbcd361e
Bert Spaan <bert.spaan@gmail.com>
2017-04-21 17:45:35 -0400

    filter by year

--- building-inspector-nyc-streets.js
+++ building-inspector-nyc-streets.js
@@ -6,6 +6,7 @@ const turf = {
   lineSegment: require('@turf/line-segment'),
   crosstrack: require('turf-crosstrack')
 }
+const edtf = require('edtf')
 const IndexedGeo = require('indexed-geo')
 
 const toTitleCase = (str) => str.replace(/\w\S*/g, (str) =>
@@ -18,8 +19,20 @@ const MAX_DISTANCE = 25
 const streetsDataset = 'nyc-streets'
 const addressesDataset = 'building-inspector'
 
-function pointLineDistance (point, line) {
-  return 100
+function getFullId (dataset, id) {
+  if (id.includes('/')) {
+    return id
+  }
+
+  return `${dataset}/${id}`
+}
+
+function getInternalId (id) {
+  if (id.includes('/')) {
+    return id.split('/')[1]
+  }
+
+  return id
 }
 
 function objectsStream (getDir, dataset, step) {
@@ -33,57 +46,59 @@ function objectsStream (getDir, dataset, step) {
 function processAddresses (indexedGeo, dirs, tools, callback) {
   console.log('      Finding closest line segments for each address')
 
+  const MS_THRESHOLD = YEAR_THRESHOLD * 365 * 24 * 60 * 60 * 1000
+
   objectsStream(dirs.getDir, addressesDataset, 'transform')
     .filter((object) => object.type === 'st:Address')
     .filter((address) => address.geometry)
     .map((address) => {
-      try {
-        const searchResults = indexedGeo.search(address.geometry)
-        const nearestResults = indexedGeo.nearest(address.geometry, 10)
-        const allResults = [...searchResults, ...nearestResults]
+      const searchResults = indexedGeo.search(address.geometry)
+      const nearestResults = indexedGeo.nearest(address.geometry, 10)
+      const allResults = [...searchResults, ...nearestResults]
 
-        const closestResults = allResults
-          // TODO: filter year threshold
-          // .filter((segment) => )
-          .map((segment) => {
-            const distance = Math.round(turf.crosstrack(address.geometry, segment, 'kilometers') * 1000)
-            return {
-              segment,
-              distance
-            }
-          })
-          .filter((segment) => segment.distance < MAX_DISTANCE)
-          .sort((a, b) => a.distance - b.distance)
+      const closestResults = allResults
+        .filter((segment) => {
+          const addressSince = edtf(String(address.validSince)).min
+          const addressUntil = edtf(String(address.validUntil)).max
 
-        if (closestResults.length) {
-          const distance = closestResults[0].distance
-          const segment = closestResults[0].segment
+          const segmentSince = edtf(String(segment.properties.validSince)).min - MS_THRESHOLD
+          const segmentUntil = edtf(String(segment.properties.validUntil)).max + MS_THRESHOLD
 
-          const name = `${address.data.number} ${toTitleCase(segment.properties.name)}`
-          // const addressId = expandURN(row.address_id)
-          // const streetId = expandURN(row.street_id)
-          // const id = addressId.split('/')[1]
-
+          return segmentSince <= addressSince && segmentUntil >= addressUntil
+        })
+        .map((segment) => {
+          const distance = Math.round(turf.crosstrack(address.geometry, segment, 'kilometers') * 1000)
           return {
-            name,
-            addressId: address.id,
-            streetId: segment.properties.id,
-            // validSince: row.validsince,
-            // validUntil: row.validuntil,
-            addressDataset: addressesDataset,
-            streetDataset: streetsDataset,
-            // streetName: row.street_name,
-            // addressData: row.address_data,
-            // lineLength: row.line_length,
-            // shortestLine: row.shortest_line,
-            // addressGeometry: row.address_geometry
+            segment,
+            distance
           }
-        } else {
-          // TODO: Nothing found! Log error to file
+        })
+        .filter((segment) => segment.distance < MAX_DISTANCE)
+        .sort((a, b) => a.distance - b.distance)
+
+      if (closestResults.length) {
+        const distance = closestResults[0].distance
+        const segment = closestResults[0].segment
+
+        const id = getInternalId(address.id)
+        const name = `${address.data.number} ${toTitleCase(segment.properties.name)}`
+
+        return {
+          id: id,
+          name,
+          addressId: getFullId(addressesDataset, address.id),
+          streetId: getFullId(streetsDataset, segment.properties.id),
+          validSince: address.validSince,
+          validUntil: address.validUntil,
+          addressDataset: addressesDataset,
+          streetDataset: streetsDataset,
+          streetName: segment.properties.name,
+          addressData: address.data,
+          lineLength: distance,
+          addressGeometry: address.geometry
         }
-      } catch (err) {
-        // TODO: log error!
-        console.log(err)
+      } else {
+        // TODO: log unmatched addresses!
       }
     })
     .compact()
@@ -129,36 +144,36 @@ function transform (config, dirs, tools, callback) {
     .split()
     .compact()
     .map(JSON.parse)
-    .map((row) => ([
+    .map((address) => ([
       {
         type: 'object',
         obj: {
-          id: row.id,
-          name: row.address,
-          type: types.address,
-          validSince: new Date(row.validSince).getFullYear(),
-          validUntil: new Date(row.validUntil).getFullYear(),
-          data: Object.assign(row.addressData, {
-            addressId: row.addressId,
-            streetId: row.streetId
+          id: address.id,
+          name: address.name,
+          type: 'st:Address',
+          validSince: address.validSince,
+          validUntil: address.validUntil,
+          data: Object.assign(address.addressData, {
+            addressId: address.addressId,
+            streetId: address.streetId
           }),
-          geometry: row.addressGeometry
+          geometry: address.addressGeometry
         }
       },
       {
         type: 'relation',
         obj: {
-          from: row.addressId,
-          to: row.streetId,
-          type: types.in
+          from: address.addressId,
+          to: address.streetId,
+          type: 'st:in'
         }
       },
       {
         type: 'relation',
         obj: {
-          from: row.id,
-          to: row.addressId,
-          type: types.sameAs
+          from: address.id,
+          to: address.addressId,
+          type: 'st:sameAs'
         }
       },
       {
@@ -166,19 +181,13 @@ function transform (config, dirs, tools, callback) {
         obj: {
           type: 'Feature',
           properties: {
-            addressId: row.addressId,
-            streetId: row.streetId,
-            streetName: row.streetName,
-            addressData: row.addressData,
-            lineLength: row.lineLength
+            addressId: address.addressId,
+            streetId: address.streetId,
+            streetName: address.streetName,
+            addressData: address.addressData,
+            lineLength: address.lineLength
           },
-          geometry: {
-            type: 'GeometryCollection',
-            geometries: [
-              row.shortestLine,
-              row.addressGeometry
-            ]
-          }
+          geometry: address.addressGeometry
         }
       }
     ]))
